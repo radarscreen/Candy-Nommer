166052b3760e15ae69dd0d2e57fe78f7effb19e8
Paris Jones <bayarette@gmail.com>
2015-02-05 10:37:00 -0800

    cleaned up/code explained

--- snake.js
+++ snake.js
@@ -4,38 +4,33 @@ Physics(function(world){
 
   var snakeVel;
 
+  //define renderer
   var renderer = Physics.renderer('canvas', {
     el: 'viewport',
     width: viewWidth,
     height: viewHeight,
     meta: false, // don't display meta data
     debug: true,
-    styles: {
-        // set colors for the circle bodies
-        'circle' : {
-          angleIndicator: 'white'
-        },
-        'rectangle' : {
-          fillStyle: '#333',
-          angleIndicator: 'white'
-        }
-    }
   });
+
+
+
   // add the renderer
   world.add( renderer );
+
   // render on each step
   world.on('step', function(){
     world.render();
   });
+
   // bounds of the window
   var viewportBounds = Physics.aabb(0, 0, viewWidth, viewHeight);
 
   // add an "apple"
   var apple = Physics.body('circle', {
-    // mass: 0,    //how does the mass affect the state of the apple? apparently it chnages the way the snake interacts with the apple
     x: Math.floor((Math.random() * 380) + 6),
     y: Math.floor((Math.random() * 380) + 6),
-    radius: 10,
+    radius: 5,
     styles: {
       strokeStyle: '#351024',
       lineWidth: 1,
@@ -47,28 +42,27 @@ Physics(function(world){
 
   world.add(apple);
 
+  // define a "newApple" --but NOT added to world yet
   var newApple = Physics.body('circle', {
-  radius: 10,
+  radius: 5,
   styles: {
     fillStyle: 'green',
     angleIndicator: 'white',
     objectType: 'apple'
     }
   });
- 
 
- //why does the radius of the apple still affect the physics engine?
 
-  console.log(newApple);
-
+  //define snake 
   var snake = Physics.body('rectangle', {
     x: 200, // x-coordinate
     y: 250, // y-coordinate
-    vx: 0.0,
+    vx: 0.0, //starts at no velocity
     vy: 0.0,
-    width: 10,
+    width: 10, //initial snake dimensions
     height: 10,
-    mass: 120000000000,
+    mass: 120000000000, //"ghetto-coded" to get around angular velocity issue
+    //snake appearance
     styles: {
       fillStyle: 'black',
       angleIndicator: 'white'
@@ -76,9 +70,14 @@ Physics(function(world){
 
   });
 
+  //snake put to sleep
   snake.sleep(true);
 
+
+  //snake added to world
   world.add(snake);
+
+
   
   // constrain objects to these bounds
   world.add(Physics.behavior('edge-collision-detection', {
@@ -88,11 +87,12 @@ Physics(function(world){
   }));
 
   // ensure objects bounce when edge collision is detected
-  world.add( Physics.behavior('body-impulse-response') );
+  world.add(Physics.behavior('body-impulse-response') );
 
   world.add(Physics.behavior('body-collision-detection'));
 
-  world.add( Physics.behavior('sweep-prune') );
+  //smooths 'sweeps' the rendering of the collision detections
+  world.add(Physics.behavior('sweep-prune') );
 
 
   // subscribe to ticker to advance the simulation
@@ -100,6 +100,8 @@ Physics(function(world){
       world.step( time );
   });
 
+
+  // user input for snake's direction changes
   window.addEventListener('keyup', function(event) {
     snake.sleep(false);
       if (event.keyCode === 37) {
@@ -116,8 +118,9 @@ Physics(function(world){
   // start the ticker
   Physics.util.ticker.start();
 
+
+  // sets constant velocity of snake
   world.on('move', function(data, e) {
-    
     var vel = snake.state.vel;
     if (data === 'left') {
       vel.set(-0.1, 0);
@@ -131,28 +134,38 @@ Physics(function(world){
       vel.set(0, 0.1);
     }
 
+    // value designated to retrieve velocity within other functions
     snakeVel = vel;
   });
 
- var counter = 0;
-  world.on('collisions:detected', function(data, e) {
-    data.collisions[0].bodyA.sleep(true);
-    data.collisions[0].bodyB.sleep(true);
+  // sets counter for scoring purposes
+  var counter = 0;
 
-   
+  // collision detection mechanism
+  world.on('collisions:detected', function(data, e) {
+      data.collisions[0].bodyA.sleep(true);
+      data.collisions[0].bodyB.sleep(true);
+
+    // stipulates that apple and snake collisions result in:
     if (snake === data.collisions[0].bodyA && apple === data.collisions[0].bodyB || apple === data.collisions[0].bodyA && snake === data.collisions[0].bodyB) {
-      console.log( "snake eats apple");
+      
+      // the apple being removed from the world,
       world.removeBody(apple);
       
+      // the newApple body has a randomly generated coordinate, 
       newApple.state.pos.set(Math.floor((Math.random() * 380)+11), Math.floor((Math.random() * 380)+11));
     
+      // the players score increases
       counter++;
 
-      
+      // the snake's velocity is maintained and the snake stays awake (does not stop upon collision),
       snake.state.vel = snakeVel;
       snake.sleep(false); 
 
+      // and add the newApple to the viewport for the snake to chase.
       world.add(newApple);
+
+
 
       //make sure to see if i can't also create ("newSnakeBit") and apend to Snake...changing colors of said bit to verify...
       // function snakeGrows(){
@@ -161,21 +174,19 @@ Physics(function(world){
       // Physics.util.extend.add(newApple);   could some manipulation of this code create a new instance of the apple?
     }
 
+    // this else block repeats the same steps 
     else if (snake === data.collisions[0].bodyA && newApple === data.collisions[0].bodyB || newApple === data.collisions[0].bodyA && snake === data.collisions[0].bodyB) {
       console.log("snake eats NEW APPLE");
       world.removeBody(newApple);
       apple.state.pos.set(Math.floor((Math.random() * 380) + 11), Math.floor((Math.random() * 380) + 11));
-      
       counter++;
-
-      
       snake.state.vel = snakeVel;
       snake.sleep(false);
-      
+      //adds initial apple
       world.add(apple);
     }
 
-
+    // if the snake collides with its own body, it dies. 
     // else if (snake === data.collisions[0].bodyA && snake === data.collisions[0].bodyB) {
     //   console.log( "snake eats self");
 
@@ -188,12 +199,15 @@ Physics(function(world){
     //     }
     // }
 
+    // if there are any other collisions (like snake with wall), snake dies.
     else  {
       console.log( "snake dies");
      
       Physics.util.ticker.stop();
     
     }
+
+    // keeps track of player score. 
     console.log("Your score is " + (counter*7));
   });
 
@@ -204,7 +218,7 @@ Physics(function(world){
 
 // compound body
 // var chainSim = function(world){
-//     // create chains...  
+//     // create chains...                                MERELY A VARIABLE I BELIEVE :/
 // }; 
 // http://wellcaffeinated.net/PhysicsJS/basic-usage
 
@@ -216,13 +230,16 @@ Physics(function(world){
 // As for the appearance, you'd need to find a way to draw that yourself with canvas. If you wanted to have the physics of a curved polygon, you'd have to write that yourself. So it's probably easier to just skin it with an image. To do that just set "body.view = myImage"
 
 // This is a bit outdated, but has some examples: http://flippinawesome.org/2013/12/02/building-a-2d-browser-game-with-physicsjs/
-// http://stackoverflow.com/questions/23668005/agregate-bodies-in-physicsjs
+ // http://stackoverflow.com/questions/23668005/agregate-bodies-in-physicsjs
+
+
+
 
 // body.before-game:after {
 //     content: 'press "z" to start';
 // }
 // body.lose-game:after {
-//     content: 'press "z" to try again';                    BEFORE AFTER GAME!  (IN HTML)
+//     content: 'press "z" to try again';                    BEFORE & AFTER GAME!  (IN HTML)
 // }
 // body.win-game:after {
 //     content: 'Win! press "z" to play again';
